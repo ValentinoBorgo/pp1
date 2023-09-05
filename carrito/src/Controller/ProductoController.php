a617e63a1c7b23d76fce995c2ca799d1fcc7fa36
valentino borgo <lmgb.1a.borgo.valentino@gmail.com>
2023-09-05 18:57:45 -0300

    Actualizacion de repository

--- carrito/src/Controller/ProductoController.php
+++ carrito/src/Controller/ProductoController.php
@@ -5,29 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Negocio\Almacen;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Producto;
+
+
 
 class ProductoController extends AbstractController
 {
  /**
  * @Route("/", name="listar_productos")
  */
- public function listarProductos(Almacen $alma): Response
+ public function listarProductos(ManagerRegistry $doctrine): Response
  {
-    $productos = $alma->findAll();
+   $repository = $doctrine->getRepository(Producto::class);
+    $productos = $repository->findAll();
 
- return $this->render('producto/lista.html.twig', ['productos' => $productos]);
+    return $this->render('producto/lista.html.twig', ['productos' => $productos]);
 
  }
 
  /**
  * @Route("/producto/{id}", name="detalle_producto")
  */
-public function detalleProducto(Almacen $alma, $id): Response
+public function detalleProducto(ManagerRegistry $doctrine, int $id): Response
 {
-   $producto = $alma->find($id);
+   $repository = $doctrine->getRepository(Producto::class);
+   $producto = $repository->find($id);
 
-return $this->render('producto/detalle.html.twig', ['productoDeta' => $producto]);
+   if (!$producto) {
+      throw $this->createNotFoundException('No existe');
+   }
+
+   return $this->render('producto/detalle.html.twig', ['productoDeta' => $producto]);
 
 }
 
--- carrito/src/DataFixtures/ProductoFixtures.php
+++ carrito/src/DataFixtures/ProductoFixtures.php
@@ -4,13 +4,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Producto;
 
 class ProductoFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i < 11; $i++) {
+            $producto = new Producto();
+            $producto->setNombre('Producto '.$i);
+            $producto->setPrecio(mt_rand(10, 100));
+            $producto->setDescripcion("Lorem ipsum dolor sit amet consectetur adipisicing elit");
+            $producto->setImagen('imagenes/producto'.$i.'.jpg');
+            $manager->persist($producto);
+            }  
 
         $manager->flush();
     }
